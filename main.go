d78308bf5a88370ea09e2f55411837d2a58dc5dc
rohan3011 <rohankamble3011@gmail.com>
2023-07-25 18:58:52 +0530

    Add chi server

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello World")
+
+	godotenv.Load()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT not found")
+	}
+
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome"))
+	})
+
+	fmt.Printf("Server running on Port:%s\n", port)
+	http.ListenAndServe(":"+port, router)
 }
