9cfbc670f34a9676f91ad3e3e6d15f79f8c50689
rohan3011 <rohankamble3011@gmail.com>
2023-10-15 10:43:35 +0530

    Added weather api

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+
 	"github.com/joho/godotenv"
 )
 
@@ -25,6 +27,36 @@ func main() {
 
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Welcome"))
+	})
+
+	router.Get("/weather", func(w http.ResponseWriter, r *http.Request) {
+		city := r.URL.Query().Get("city")
+		latLong, err := getLatLong(city)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(fmt.Sprintf("failed to get Latitude and Longitude %s", err)))
+			return
+		}
+
+		weather, err := getWeather(*latLong)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(fmt.Sprintf("failed to get weather %s", err)))
+			return
+		}
+
+		fmt.Println(weather)
+
+		weatherDisplay, err := extractWeatherData(city, weather)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(fmt.Sprintf("failed to extract weather data %s", err)))
+			return
+		}
+		w.Write([]byte(fmt.Sprintf("weather: %s", weather)))
+		t, _ := template.ParseFiles("views/weather.html")
+		t.Execute(w, weatherDisplay)
+
 	})
 
 	fmt.Printf("Server running on Port:%s\n", port)
---
+++ weather.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+type LatLong struct {
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
+
+type GeoResponse struct {
+	Results []LatLong `json:"results"`
+}
+
+type WeatherResponse struct {
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+	Timezone  string  `json:"timezone"`
+	Hourly    struct {
+		Time          []string  `json:"time"`
+		Temperature2m []float64 `json:"temperature_2m"`
+	} `json:"hourly"`
+}
+
+type WeatherDisplay struct {
+	City      string
+	Forecasts []Forecast
+}
+
+type Forecast struct {
+	Date        string
+	Temperature string
+}
+
+// Gets Latitude and Longitude for a given city
+func getLatLong(city string) (*LatLong, error) {
+	endpoint := fmt.Sprintf("https://geocoding-api.open-meteo.com/v1/search?name=%s&count=1&language=en&format=json", city)
+
+	resp, err := http.Get(endpoint)
+	if err != nil {
+		return nil, fmt.Errorf("error making request to Geo API: %w", err)
+	}
+
+	defer resp.Body.Close()
+
+	var response GeoResponse
+	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
+		return nil, fmt.Errorf("error Decoding response %w", err)
+	}
+
+	if len(response.Results) < 1 {
+		return nil, errors.New("no result found")
+	}
+
+	return &response.Results[0], nil
+}
+
+// Gets the weather
+func getWeather(latLong LatLong) (string, error) {
+	endpoint := fmt.Sprintf("https://api.open-meteo.com/v1/forecast?latitude=%.6f&longitude=%.6f&hourly=temperature_2m", latLong.Latitude, latLong.Longitude)
+
+	resp, err := http.Get(endpoint)
+	if err != nil {
+		return "", fmt.Errorf("error fetching weather %w", err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("error reading response %w", err)
+	}
+
+	return string(body), nil
+}
+
+func extractWeatherData(city string, rawWeather string) (WeatherDisplay, error) {
+	var weatherResponse WeatherResponse
+	if err := json.Unmarshal([]byte(rawWeather), &weatherResponse); err != nil {
+		return WeatherDisplay{}, fmt.Errorf("error decoding weather response: %w", err)
+	}
+
+	var forecasts []Forecast
+	for i, t := range weatherResponse.Hourly.Time {
+		date, err := time.Parse(time.RFC3339, t)
+		if err != nil {
+			return WeatherDisplay{}, fmt.Errorf("failed to parse time: %s", err)
+		}
+		forecast := Forecast{
+			Date:        date.Format("Mon 15:04"),
+			Temperature: fmt.Sprintf("%.1f°C", weatherResponse.Hourly.Temperature2m[i]),
+		}
+		forecasts = append(forecasts, forecast)
+	}
+	return WeatherDisplay{
+		City:      city,
+		Forecasts: forecasts,
+	}, nil
+}
